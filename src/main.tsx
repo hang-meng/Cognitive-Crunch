8da89c4509b484dbf41a36fdefe27fffc67599d6
hang-meng <2036327633@qq.com>
2026-05-16 21:58:56 +0800

    chore: 移除 PWA 支持（manifest/service worker）

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// 注册 PWA Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(
-      () => console.log('SW registered'),
-      (err) => console.log('SW registration failed:', err)
-    )
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
